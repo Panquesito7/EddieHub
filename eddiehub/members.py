c15273b61bd3a1468d1ecdf0d7f68b75c26e0e4e
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-09-12 17:57:48 +0530

    exception

--- eddiehub/members.py
+++ eddiehub/members.py
@@ -7,8 +7,18 @@ def member(username):
             f"https://raw.githubusercontent.com/EddieHubCommunity/LinkFree/main/public/data/{username}.json"
         ).json()
     except:
+        orgs = requests.get(f"https://api.github.com/users/{username}/orgs").json()
+        joined = False
+        for org in orgs:
+            if "EddieHubCommunity" in org["login"]:
+                joined = True
+        if not joined:
+            error = "User not join EddieHub. Please join on https://github.com/EddieHubCommunity " \
+            "if already joined change visibility to public."
+        else:
+            error = "User not found, Add data in https://github.com/EddieHubCommunity/LinkFree"
         details = {
-            "error": "User not found, Add data in https://github.com/EddieHubCommunity/LinkFree"
+            "error": error
         }
     return details
 

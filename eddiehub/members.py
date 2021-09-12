69ef8905030215ae082270adb6181222c169274d
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-09-12 17:30:46 +0530

    Update members.py

--- eddiehub/members.py
+++ eddiehub/members.py
@@ -2,7 +2,12 @@ import requests
 
 
 def member(username):
-    details = requests.get(
-        f"https://raw.githubusercontent.com/EddieHubCommunity/LinkFree/main/public/data/{username}.json"
-    ).json()
+    try:
+        details = requests.get(
+            f"https://raw.githubusercontent.com/EddieHubCommunity/LinkFree/main/public/data/{username}.json"
+        ).json()
+    except:
+        details = {
+            "error": "User not found, Add data in https://github.com/EddieHubCommunity/LinkFree"
+        }
     return details

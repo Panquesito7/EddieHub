1253ede71902323ffadc2cf803e02df0c72c38df
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-09-12 18:06:24 +0530

    Added eddiehub

--- eddiehub/__init__.py
+++ eddiehub/__init__.py
@@ -4,4 +4,5 @@
 # All rights reserved by FayasNoushad
 # License -> https://github.com/FayasNoushad/EddieHub/blob/main/LICENSE
 
+from .eddiehub import eddiehub
 from .members import member, members
